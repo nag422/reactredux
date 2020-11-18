e3c1609bfae369128c3ca573c1f2190489b2018d
nag422 <nagendrakumar422@gmail.com>
2020-11-18 17:24:27 +0530

    models completed

---
+++ controllers/posts.js
@@ -0,0 +1,24 @@
+const PostMessage = require('../models/postMessage');
+
+exports.getPosts = async (req,res) => {
+    try{
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages)
+    }
+    catch(e) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+exports.createPost = async (req,res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+}
--- index.js
+++ index.js
@@ -10,8 +10,22 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const postRoutes = require('./routes/posts');
 const app = express();
+
+
 
 app.use(bodyParser.json({limit:"30mb", extended:  true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:  true}))
 app.use(cors());
+
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL = 'mongodb://127.0.0.1:27017/reactreduxnode';
+const PORT = process.env.PORT || 5000; 
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+.catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
